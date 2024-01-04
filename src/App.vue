e0b2021f67dc057ac44c3afca457f7fdf2f4aa41
Abe Reyes <abe.raise@gmail.com>
2024-01-04 13:20:28 -0500

    1.0.0 fresh initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from './components/TheForm.vue';
+
+export default {
+  components: {
+    TheForm
+  }  
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292928;
+}
+
+h1 {
+  margin: 0;
+  padding: 1rem;
+  color: #fff;
+  background-color: #292928;
+  text-align: center;
+}
+</style>
